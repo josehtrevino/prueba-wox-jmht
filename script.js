ead11f61636b49d2327668510627e130a0c5b0e8
Jose Hernandez <josemht.94@gmail.com>
2021-06-10 20:16:50 -0500

    colors, effects and details added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//console.log('hello world!');
+const buttons = document.querySelectorAll('.job__reward--btn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = button.parentNode.nextElementSibling;
+    target.classList.toggle('hidden');
+  });
+});
